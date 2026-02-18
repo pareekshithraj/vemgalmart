d329168180aff6146bc3f08f02b97d0f1f4a4b4a
Pareekshith Raj <115618649+pareekshithraj@users.noreply.github.com>
2026-02-18 21:29:15 +0530

    Add files via upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,14 +21,18 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // Frontend URL
+        origin: process.env.CLIENT_URL || ["http://localhost:5173", "https://vemgalmart-pareekshithraj.vercel.app", "https://vemgalmart.vercel.app"],
         methods: ["GET", "POST"]
     }
 });
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// Update standard CORS as well
+app.use(cors({
+    origin: process.env.CLIENT_URL || ["http://localhost:5173", "https://vemgalmart-pareekshithraj.vercel.app", "https://vemgalmart.vercel.app"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Share io instance
